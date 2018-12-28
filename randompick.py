ad7f6e75813cff387011284ef7774abfc41aa6ef
Blake Bahrenburg <35677194+Bwbahrenburg@users.noreply.github.com>
2018-12-28 15:31:08 -0600

    Update randompick.py

--- randompick.py
+++ randompick.py
@@ -1,25 +1,44 @@
 import random
 
 itemlist = []
-x =0
+x=0
+
+print("\n\n\n\n\n This program will randomly choose something out of a list you make. \n")
 
 while x != 'q':
-	x = input("Enter an item to list or type 'q' to exit \n")
+	x = input("\nEnter an item to list or type 'q' to exit \n")
+
 	if x != 'q':
-		itemlist.append(x)
+		if x  == '':
+			print("\n\nYou need to enter something into the list\n")
+			x = 0
+		else:
+			itemlist.append(x)
+# Checking if choice is a duplicate
+			if len(set(itemlist)) == len(itemlist):
+				x=0
+# Deletes duplicate if applicable
+			else:
+				print(f"\n\n {x} is already in the list enter another item\n\n")
+				del itemlist[-1]
+				x = 0
 	else:
 		break
-		
-def Results():	
-    results = random.choice(itemlist)
-    print(f"\n You pick ", results)
-    
+
+# Random pick from list function
+def Results():
+	results = random.choice(itemlist)
+	print(f"\n You pick *****  {results}  *****")
+
 Results()
-again = input("\n Would you like another choice 'y' or 'n' ?  ")
+
+# Repeat the pick option
+again = input("\n Would you like to make another choice 'y' or 'n' ? ")
 
 while again == 'y':
 	Results()
-	again = input("\n Again 'y' or 'n'")
+	again = input("\n Again 'y' or 'n' ")
 
 else:
-	print("Thanks for playing")
+	print("\n\nThanks\n\n")
+
